c38f5c13e5379db6369191bd01d2300b1e9a1c96
Anton Spaskyi <anton.spaskyi@onevisionresources.com>
2024-05-24 13:22:47 +0200

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={inter.className}
+        style={{
+          background: "none",
+        }}
+      >
+        {children}
+      </body>
     </html>
   );
 }
