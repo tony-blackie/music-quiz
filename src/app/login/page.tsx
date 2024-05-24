aa8fd931f48549a71a1f5675d82df845648469ee
Anton Spaskyi <anton.spaskyi@onevisionresources.com>
2024-05-24 14:32:06 +0200

    add pallete and simple bootstrap for login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,119 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex font-sans">
+      <div className="relative flex-none w-48">
+        <img
+          src="/classic-utility-jacket.jpg"
+          alt=""
+          className="absolute inset-0 object-cover w-full h-full"
+          loading="lazy"
+        />
+      </div>
+      <form className="flex-auto p-6">
+        <div className="flex flex-wrap">
+          <h1 className="flex-auto text-lg font-semibold text-slate-900">
+            Utility Jacket
+          </h1>
+          <div className="text-lg font-semibold text-slate-500">$110.00</div>
+          <div className="flex-none w-full mt-2 text-sm font-medium text-slate-700">
+            In stock
+          </div>
+        </div>
+        <div className="flex items-baseline pb-6 mt-4 mb-6 border-b border-slate-200">
+          <div className="flex space-x-2 text-sm">
+            <label>
+              <input
+                className="sr-only peer"
+                name="size"
+                type="radio"
+                value="xs"
+                checked
+              />
+              <div className="flex items-center justify-center rounded-lg w-9 h-9 text-mango-50 peer-checked:font-semibold peer-checked:bg-mango-700 hover:peer-checked:bg-mango-800 peer-checked:text-white">
+                XS
+              </div>
+            </label>
+            <label>
+              <input
+                className="sr-only peer"
+                name="size"
+                type="radio"
+                value="s"
+              />
+              <div className="flex items-center justify-center rounded-lg w-9 h-9 text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+                S
+              </div>
+            </label>
+            <label>
+              <input
+                className="sr-only peer"
+                name="size"
+                type="radio"
+                value="m"
+              />
+              <div className="flex items-center justify-center rounded-lg w-9 h-9 text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+                M
+              </div>
+            </label>
+            <label>
+              <input
+                className="sr-only peer"
+                name="size"
+                type="radio"
+                value="l"
+              />
+              <div className="flex items-center justify-center rounded-lg w-9 h-9 text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+                L
+              </div>
+            </label>
+            <label>
+              <input
+                className="sr-only peer"
+                name="size"
+                type="radio"
+                value="xl"
+              />
+              <div className="flex items-center justify-center rounded-lg w-9 h-9 text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+                XL
+              </div>
+            </label>
+          </div>
+        </div>
+        <div className="flex mb-6 space-x-4 text-sm font-medium">
+          <div className="flex flex-auto space-x-4">
+            <button
+              className="h-10 px-6 font-semibold text-white bg-black rounded-md"
+              type="submit"
+            >
+              Buy now
+            </button>
+            <button
+              className="h-10 px-6 font-semibold border rounded-md border-slate-200 text-slate-900"
+              type="button"
+            >
+              Add to bag
+            </button>
+          </div>
+          <button
+            className="flex items-center justify-center flex-none border rounded-md w-9 h-9 text-slate-300 border-slate-200"
+            type="button"
+            aria-label="Like"
+          >
+            <svg width="20" height="20" fill="currentColor" aria-hidden="true">
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+              />
+            </svg>
+          </button>
+        </div>
+        <p className="text-sm text-slate-700">
+          Free shipping on all continental US orders.
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,21 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      mango: {
+        "50": "#fff9eb",
+        "100": "#fdecc8",
+        "200": "#fad78d",
+        "300": "#f9c668",
+        "400": "#f6a529",
+        "500": "#f08310",
+        "600": "#d45f0b",
+        "700": "#b0410d",
+        "800": "#8f3211",
+        "900": "#762a11",
+        "950": "#441204",
+      },
+    },
   },
   plugins: [],
 };
