a28155ab972e713499d14669e0d9905487d30231
Anton Spaskyi <anton.spaskyi@onevisionresources.com>
2024-05-25 15:24:32 +0200

    add input and paragraph

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,52 +3,24 @@ import { ButtonFlat, ButtonOutline } from "@/components";
 const Login = () => {
   return (
     <div className="flex font-sans">
-      <div className="relative flex-none w-48">
-        <img
-          src="/classic-utility-jacket.jpg"
-          alt=""
-          className="absolute inset-0 object-cover w-full h-full"
-          loading="lazy"
-        />
-      </div>
-      <form className="justify-center flex-auto p-6 bg-slate-950 align-center">
+      <form className="justify-center flex-auto p-6 bg-mango-950 align-center">
         <ButtonFlat text={"Default"} disabled={true} />
         <ButtonOutline text={"Active"} disabled={true} />
 
-        <h1 className="text-xl font-bold text-center">Welcome!</h1>
+        <h1 className="text-xl font-bold text-center text-mango-50">
+          Welcome!
+        </h1>
 
-        <div className="flex mb-6 space-x-4 text-sm font-medium">
-          <div className="flex flex-auto space-x-4">
-            <button
-              className="h-10 px-6 font-semibold text-white bg-black rounded-md"
-              type="submit"
-            >
-              Buy now
-            </button>
-            <button
-              className="h-10 px-6 font-semibold border rounded-md border-slate-200 text-slate-900"
-              type="button"
-            >
-              Add to bag
-            </button>
-          </div>
-          <button
-            className="flex items-center justify-center flex-none border rounded-md w-9 h-9 text-slate-300 border-slate-200"
-            type="button"
-            aria-label="Like"
-          >
-            <svg width="20" height="20" fill="currentColor" aria-hidden="true">
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-              />
-            </svg>
-          </button>
-        </div>
-        <p className="text-sm text-slate-700">
-          Free shipping on all continental US orders.
+        <p className="text-sm text-center text-mango-50">
+          Please enter the name and let's start our quiz!
         </p>
+
+        <input
+          className="w-full py-2 pl-10 text-sm leading-6 rounded-md shadow-sm appearance-none focus:ring-1 focus:ring-mango-400 focus:outline-none text-mango-950 placeholder-mango-400 ring-1 ring-mango-200"
+          type="text"
+          aria-label="Name"
+          placeholder="Type your name here"
+        />
       </form>
     </div>
   );
