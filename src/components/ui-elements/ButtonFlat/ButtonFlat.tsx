d98aefa352f146b6e206ffa16e900734077abb19
Anton Spaskyi <anton.spaskyi@onevisionresources.com>
2024-05-26 17:59:53 +0200

    apply styling improvements to login form

--- src/components/ui-elements/ButtonFlat/ButtonFlat.tsx
+++ src/components/ui-elements/ButtonFlat/ButtonFlat.tsx
@@ -6,7 +6,7 @@ type Props = {
 const ButtonFlatActive = ({ text, disabled = false }: Props) => {
   return (
     <button
-      className="flex items-center justify-center w-64 h-10 my-2 font-semibold rounded-lg bg-mango-600 text-mango-50 hover:bg-mango-700 disabled:bg-mango-100 disabled:text-mango-400"
+      className="flex items-center justify-center w-64 h-10 my-2 font-medium rounded-lg bg-mango-600 text-mango-50 hover:bg-mango-700 disabled:bg-slate-100 disabled:text-slate-400"
       disabled={disabled}
     >
       {text}
