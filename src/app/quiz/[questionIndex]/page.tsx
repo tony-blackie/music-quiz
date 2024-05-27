9aeec3b40478d6da7c122516f86b7057e3357cdd
Anton Spaskyi <anton.spaskyi@onevisionresources.com>
2024-05-27 16:42:06 +0200

    add request for the question data

--- src/app/quiz/[questionIndex]/page.tsx
+++ src/app/quiz/[questionIndex]/page.tsx
@@ -2,17 +2,33 @@ import { API_URL, SONGS_COUNT_PER_QUESTION } from "@/constants";
 import Quiz from "./Quiz";
 import { getRandomValueFromZeroToNum } from "./utils";
 
-const QuizPage = ({ params }: { params: { questionIndex: string } }) => {
+const getQuestion = async (questionIndex: string | number) => {
+  const res = await fetch(`${API_URL}/question/${questionIndex}`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch the question data");
+  }
+
+  return res.json();
+};
+
+export default async function QuizPage({
+  params,
+}: {
+  params: { questionIndex: string };
+}) {
   const questionIndex = parseInt(params.questionIndex, 10);
   const randomSongIndex = getRandomValueFromZeroToNum(SONGS_COUNT_PER_QUESTION);
 
   const audioUrl = `${API_URL}/songs/${questionIndex}-${randomSongIndex}.mp3`;
+
+  const question = await getQuestion(questionIndex);
+
+  console.log("question: ", question);
 
   return (
     <div className="flex w-4/5 px-12 py-24 font-sans rounded-2xl h-4/5 bg-mango-950">
       <Quiz audioUrl={audioUrl} questionIndex={questionIndex} />;
     </div>
   );
-};
-
-export default QuizPage;
+}
