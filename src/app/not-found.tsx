dda39734263b2df4a2bc3e26740e1103b40d6d37
Anton Spaskyi <anton.spaskyi@onevisionresources.com>
2024-06-12 15:10:28 +0200

    fix non-escaped character

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div>
       <h2>Not Found Page</h2>
-      <p>Page doesn't exists</p>
+      <p>{`Page doesn't exist`}</p>
       <Link href="/">Go to Home</Link>
     </div>
   );
