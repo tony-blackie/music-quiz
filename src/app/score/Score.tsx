1c27d22288b582ff7b6132a4b06d687f323f52d5
Anton Spaskyi <anton.spaskyi@onevisionresources.com>
2024-06-12 15:53:49 +0200

    fix non-escaped character

--- src/app/score/Score.tsx
+++ src/app/score/Score.tsx
@@ -7,33 +7,37 @@ import Link from "next/link";
 import { getStoreValues } from "../utils/store";
 import { useSyncExternalStore } from "react";
 import PageContainer from "@/components/ui-elements/PageContainer/PageContainer";
+import dynamic from "next/dynamic";
 
-const Score = () => {
-  const store = useSyncExternalStore(subscribe, getSnapshot);
-  const { name, count } = getStoreValues(store);
+const Score = dynamic(
+  () => {
+    const store = useSyncExternalStore(subscribe, getSnapshot);
+    const { name, count } = getStoreValues(store);
 
-  const handleButtonClick = () => {
-    QuizSerializer.clear();
-  };
+    const handleButtonClick = () => {
+      QuizSerializer.clear();
+    };
 
-  return (
-    <PageContainer>
-      <div className="w-full h-full flex flex-col justify-around bg-mango-950 text-mango-50">
-        <div>
-          <div className="font-bold px-8 uppercase text-center">{name}</div>
-          <div className="text-center">You got {count} out of 12 points!</div>
+    return (
+      <PageContainer>
+        <div className="w-full h-full flex flex-col justify-around bg-mango-950 text-mango-50">
+          <div>
+            <div className="font-bold px-8 uppercase text-center">{name}</div>
+            <div className="text-center">You got {count} out of 12 points!</div>
+          </div>
+          <div className="w-full flex justify-center">
+            <Link
+              href={Routes.QUIZ + `/${INITIAL_QUESTION_INDEX}`}
+              className="w-44"
+            >
+              <ButtonFlat text="Restart Quiz" onClick={handleButtonClick} />
+            </Link>
+          </div>
         </div>
-        <div className="w-full flex justify-center">
-          <Link
-            href={Routes.QUIZ + `/${INITIAL_QUESTION_INDEX}`}
-            className="w-44"
-          >
-            <ButtonFlat text="Restart Quiz" onClick={handleButtonClick} />
-          </Link>
-        </div>
-      </div>
-    </PageContainer>
-  );
-};
+      </PageContainer>
+    );
+  },
+  { ssr: false },
+);
 
 export default Score;
